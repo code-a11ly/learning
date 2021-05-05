c69053c0a0db8200865988d3a4c627aa2fb5220a
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-04 23:38:03 -0300

    I created and made a variable type description of c++

---
+++ c++/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // VARIABLE TYPES:
+
+    int i = 1;                  // interger type variable
+    float pi = 3.1456;          // floating point number variable
+    double size = 1.66666666;   // double precision point number variable
+    char character = 'a';       // single string character
+    string name = "robert";     // string (aka text type variable)
+    bool lang = true;           // bool (aka true or false variable)
+   
+
+    return 0;
+}
