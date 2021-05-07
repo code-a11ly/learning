3d737846fa3f4429208d3d5af54161c3473311e9
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-06 21:17:35 -0300

    I put some spacing between the varibles and descriptions of the last

--- c++/variables.cpp
+++ c++/variables.cpp
@@ -6,12 +6,23 @@ int main()
 
     // VARIABLE TYPES:
 
-    int i = 1;                  // interger type variable
-    float pi = 3.1456;          // floating point number variable
-    double size = 1.66666666;   // double precision point number variable
-    char character = 'a';       // single string character
-    string name = "robert";     // string (aka text type variable)
-    bool lang = true;           // bool (aka true or false variable)
+    // interger type variable
+    int i = 1;                  
+
+    // floating point number variable
+    float pi = 3.1456;          
+
+    // double precision point number variable
+    double size = 1.66666666;   
+
+    // single string character
+    char character = 'a';       
+
+    // string (aka text type variable)
+    string name = "robert";     
+
+    // bool (aka true or false variable)
+    bool lang = true;
    
 
     return 0;
