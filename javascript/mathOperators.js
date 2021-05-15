18dc5370fe9411b76c69fedfd1e6bb8c538e9d42
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-14 23:52:31 -0300

    I added a file exaplaining the basic math operators in js

---
+++ javascript/mathOperators.js
@@ -0,0 +1,31 @@
+// ARITHMETIC OPERATORS
+
+/*
+    A computer is simply a more "smart" calculator
+    and to think of programing without doing simple
+    operations is unlogic
+
+    so the Math or Arithemetic Operators are:
+
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    % for getting the interger value that resulted
+        from a division between a odd and a even number
+*/
+
+// and following there are some examples:
+
+var addition = 3 + 2;
+var subtraction = 3 - 70;
+var division = 10 / 2;
+var multiplication = 3 * 10;
+var interger = 3 % 2;
+
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplication);
+console.log(interger);
