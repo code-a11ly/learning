f95c463cc31f82c14bd59afaa66084d2d2babd7e
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-14 23:53:48 -0300

    I finished the variables.js by explaining about all the variables

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,7 +1,32 @@
 
 // VARIABLE TYPES IN JAVASCRIPT
 
-var name = "john";
-var age = 19;
-var married = false;
-var profession = undefined;
+/*
+    In JavaScript (Js) differently to other
+    prrograming languages we don't declare the 
+    variable type, but let the own language
+    interpret which kind of variable we are
+    using
+*/
+
+var name = "john";  // this is a string type variable
+var age = 19;   // this is a interger type variable
+var married = false;    // this is a boolean type variable
+var profession = undefined;     // this is a non define variable
+
+var person = {                      // we have the object type
+    name: "gabriel",                // variables as well
+    age: 19,                        // they can store a lot more
+    hasDriversLicense: false,       // information than a single variable
+    dateOfBirth: "10/01/2002",      // and in a much more detailed way
+
+    address: {                      // we can even create an object
+        firstLine: "123",           // inside another object
+        postCode: "SE1",
+        country: "Brazil"
+
+    }
+
+};
+
+
