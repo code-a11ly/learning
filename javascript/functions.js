5cbd1467fa3a38814a69df74b67bfbfcafede2cd
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-16 16:10:50 -0300

    I added a file explaining basic function but I still have to finish it

---
+++ javascript/functions.js
@@ -0,0 +1,16 @@
+// FUNCTIONS IN JAVASCRIPT
+
+/*
+    okay so when you want to execute a code
+    but what if you have to execute the same
+    code repeated times along the code? that
+    is where functions come in
+*/
+
+function soma(a, b) {
+
+
+    return (a + b);
+};
+
+var result = soma(1, 2)
