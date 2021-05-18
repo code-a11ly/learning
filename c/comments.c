d8dde50c41552ce39beb35debbfd163c4ebed232
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-17 23:58:46 -0300

    I added a code explaining comments in c

---
+++ c/comments.c
@@ -0,0 +1,26 @@
+
+// COMMNETS IN C:
+#include <stdio.h>
+
+int main()
+{
+    
+
+    // this is a single line command
+
+    /*
+    this
+    is
+    a
+    multiple
+    line
+    comment
+    */
+
+    /*
+    And as you can see it does not interfere with
+    the typed code
+    */
+
+    return 0;
+}
