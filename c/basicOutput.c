3b86c6651f7c6084509da26eba3c7b1b26a96658
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-17 23:59:42 -0300

    I added a code explaining the basic output in c

---
+++ c/basicOutput.c
@@ -0,0 +1,30 @@
+
+// SIMPLE OUTPUT AND ENDING LINE COMMANDS IN C:
+#include <stdio.h>
+
+int main()
+{
+
+    /*
+    To print something into the console we use the
+    printf open up a parenthesis and put what we want
+    to print inside.
+    NOTE THAT:
+
+    "" - we use double quotation marks to print
+         a string or aka simple sentence
+
+    \n - is used to say print the content after the \n
+         in a New line
+
+    ; - the semicolon is used to specify to the compiler
+        that the typed line of code ended there, DON NOT
+        FORGET TO WRITE IT after ending a code line otherwise
+        you will get an error!!
+
+    */
+    // this is how you print something into the console
+    printf("\nHello World!\n");
+
+    return 0;
+}
