6d4f0a7b2774c653180c4bba2cff6295c8bb9660
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-17 23:59:10 -0300

    I added a code explaining the variable type in c

---
+++ c/variableTypes.c
@@ -0,0 +1,25 @@
+// VARIABLE TYPES IN C:
+#include <stdio.h>
+
+int main()
+{
+
+    /*
+    In every programing language we have variables
+    that if you don't know are kind of virtual boxes
+    that instead of storing things store values.
+    The C variables are:
+
+    int - for interger numbers
+    float - for floating point numbers
+    double - for double precision floating point numbers
+    char - for characters
+    */
+
+    int num = 1;
+    float nota = 3.3;
+    double pi = 3.14e4;     // 3,14 * 10^4
+    char a = 'a';
+
+    return 0;
+}
