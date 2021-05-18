4ed8df8a3bd65b9431361752019829ea3557c1e1
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-17 23:58:10 -0300

    I added a code explaining the basic structure of a c program

---
+++ c/basicStructure.c
@@ -0,0 +1,23 @@
+
+// BASIC COMMAND STRUCTURE IN C:
+
+#include <stdio.h>      // to do something inside c you need
+                        // the include a library into your code
+                        // the stdio for example is the standart
+                        // input and output library
+
+int main()    // the main function is the most important
+              // funtion of the code cuz here is where all
+              // the code of your program will be really
+              // executed
+{
+    /*
+    ALL THE CODE
+    THAT YOU NEED TO EXECUTE
+    GOES INSIDE HERE
+    */
+
+    return 0;      // the return 0 in main is used to
+                   // show the code that if it returns 0
+                   // then everything went well
+}
