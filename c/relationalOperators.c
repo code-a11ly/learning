07b070cbf0f66bd43605bafcb58927d3eab2eda5
code-a11ly <gabrielsouza10042002@gmail.com>
2021-05-20 23:41:42 -0300

    I added a C relational operators notes but it's not complete

---
+++ c/relationalOperators.c
@@ -0,0 +1,39 @@
+// VARIABLE TYPES IN C:
+#include <stdio.h>
+
+int main()
+{
+
+    // RELATIONAL OPERATORS IN C:
+
+    /*
+    In all the languages we have the relational
+    operators that basically compare things and
+    gives us a answer so in C:
+
+    && stands for AND relational operator
+
+    || stands for OR relational operator
+
+    > stands for greater than
+
+    < stands for lessers than
+
+    = stands for equal to
+
+    There are too the:
+
+    => equal OR greater than
+
+    =< equal OR lesser than
+
+    */
+
+    int r, x = 5, y = 3;
+
+    r = (x > 2) && (y > 0);
+
+    printf("Result: %d\n", r);
+
+    return 0;
+}
